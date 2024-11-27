18dd19a3a8b5ed15e6baa88e444028354359a539
edgmsh <edgmsh@gmail.com>
2024-11-27 17:29:27 -0500

    Apply Styling to the Photo Component

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -8,16 +8,16 @@ const PhotoListItem = (props) => {
   const { id, location, imageSource, username, profile } = props.photo;
 
   return (
-    <div>
-      <img src={imageSource} alt="Photo"/>
-      <p>
-        <img src={profile} alt="Profile"/>
-      </p>  
-      
-        <div>
-          <p>{username} </p>
-          <p>{location.city} {location.country}</p>
+    <div className="photo-list__item">
+      <img src={imageSource} alt="Photo" className="photo-list__image"/>
+
+      <div className="photo-list__user-details">
+        <img src={profile} alt="Profile" className="photo-list__user-profile"/>
+        <div className="photo-list__user-info">
+          <p>{username}</p>
+          <p className="photo-list__user-location ">{location.city}, {location.country}</p>
         </div>
+      </div>
     </div>
   )
 };
