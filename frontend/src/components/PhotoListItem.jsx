e2ce285f9887d3aaf2d80f4085dc0ca6542c9ca8
edgmsh <edgmsh@gmail.com>
2024-11-27 17:10:46 -0500

    Building to Display a Photo

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,19 +3,23 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
+const PhotoListItem = (props) => {
   /* Insert React */
+  const { id, location, imageSource, username, profile } = props.photo;
+
+  return (
+    <div>
+      <img src={imageSource} alt="Photo"/>
+      <p>
+        <img src={profile} alt="Profile"/>
+      </p>  
+      
+        <div>
+          <p>{username} </p>
+          <p>{location.city} {location.country}</p>
+        </div>
+    </div>
+  )
 };
 
 export default PhotoListItem;
