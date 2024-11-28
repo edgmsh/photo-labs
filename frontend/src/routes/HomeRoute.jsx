246c0070b0401edc02ee2eb9e2c9689bf59747a6
edgmsh <edgmsh@gmail.com>
2024-11-28 17:53:06 -0500

    Create the Home Page Layout

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
-
 import '../styles/HomeRoute.scss';
+import TopNavigationBar from '../components/TopNavigationBar';
+import PhotoList from '../components/PhotoList';
 
-const HomeRoute = () => {
+function HomeRoute(props) {
+
+  const {favPhotoExist, like, toggleModalState, PhotoData, clickImgSetID, TopicData, selectTopic} = props;
+
   return (
     <div className="home-route">
-      {/* Insert React */}
+      <TopNavigationBar favPhotoExist={favPhotoExist} TopicData={TopicData} selectTopic={selectTopic} />
+      <PhotoList photos={PhotoData} 
+      like={like} 
+      toggleModalState={toggleModalState}
+      clickImgSetID={clickImgSetID} />
     </div>
   );
-};
+}
 
 export default HomeRoute;
